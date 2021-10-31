531afb71102071a071038e2a7871a55011989d64
nurhene <oueslatinnourhene@gmail.com>
2021-10-31 22:43:36 +0100

    test updates

--- src/test/java/tn/esprit/spring/EmployeServiceTest.java
+++ src/test/java/tn/esprit/spring/EmployeServiceTest.java
@@ -30,6 +30,52 @@ import tn.esprit.spring.services.IEmployeService;
 		e = ems.ajouterEmploye(e);
 		assertNotNull(e);
 	}
+
+	@Test
+	void testGetAllEmployes() {
+		List<Employe> le=ems.getAllEmployes();
+		le.forEach(e->l.info(e+"\n"));
+		assertNotNull(le);
+	}
+	
+	@Test
+	void testGetEmployeById(){
+		ems.getEmployeById(17);
+		assertNotNull(ems.getEmployeById(17));
+	}
+	
+	/*@Test 
+	void testDeleteEmployeById(){
+		ems.deleteEmployeById(25);
+	}*/
+	
+	@Test
+	void testupdateemailEmploye(){
+		ems.getEmployeById(25);
+		assertNotNull(ems.getEmployeById(25));
+		ems.mettreAjourEmailByEmployeId("nourhene.oueslati@esprit.tn", 25);
+	}
+
+	@Test
+	void testaffecterEmployeADepartement() {
+		ems.getEmployeById(18);
+		depts.findById(1);
+		ems.affecterEmployeADepartement(18, 1);
+	}
+	
+	@Test
+	void testdesaffecterEmployeADepartement() {
+		ems.getEmployeById(17);
+		depts.findById(2);
+		ems.desaffecterEmployeDuDepartement(17, 2);
+	}
+	
+	@Test
+	void testGetEmployeprenomById(){
+		Employe e=ems.getEmployeById(17);
+	//	System.out.print(e.getPrenom());
+		assertNotNull(e.getPrenom());
+	}
 	
 	
 }
